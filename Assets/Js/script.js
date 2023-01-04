52a797f24a0512b354ea9d6d990996fbcf003b8a
dirtnasty jones <jeremymccarter27@gmail.com>
2023-01-04 15:05:11 -0800

    added functions for layout and array for answers

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,17 +1,68 @@
 var timerElement =  document.getElementById("timer");
 var startButton = document.getElementById("start");
-var sec = 75;
 var questions  = document.querySelector(".questions");
 var hideQs = document.getElementById("hideQs")
-function timer(){
+var challenge = [{
     
+    Q:"What is an ordered list of chars?",
+    A:[
+"A1","A2","A3","A4"
+    ]
+
+},
+{
+    
+    Q:"What is an ordered list of chars?",
+    A:[
+"A1","A2","A3","A4"
+    ]
+
+},
+{
+    
+    Q:"What is an ordered list of chars?",
+    A:[
+"A1","A2","A3","A4"
+    ]
+
+},
+{
+    
+    Q:"What is an ordered list of chars?",
+    A:[
+"A1","A2","A3","A4"
+    ]
+
+},
+{
+    
+    Q:"What is an ordered list of chars?",
+    A:[
+"A1","A2","A3","A4"
+    ]
+
+},
+{
+    
+    Q:"What is an ordered list of chars?",
+    A:[
+"A1","A2","A3","A4"
+    ]
+
+},
+
+]
+function timer(time){
+    var sec = time;
     var timer = setInterval(function(){
        timerElement.innerHTML='00:'+sec;
         sec--;
-        if (sec < 0) {
+        if (sec < 0) 
+        {gameOver();
             clearInterval(timer);
         }
     }, 1000);
+   
 }
 
 startButton.addEventListener("click", startGame );
@@ -22,3 +73,26 @@ timer();
 hideQs.style.display = "none";
 
 }
+
+
+function wrongAnswer(){
+
+}
+
+
+function correctAnswer(){
+
+
+
+}
+
+function checkAnswer(){
+
+
+
+
+}
+
+challenge.forEach(Q => {
+    
+});
